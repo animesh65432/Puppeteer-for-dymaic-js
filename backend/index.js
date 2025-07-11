aaef995a44995be6b94a338cff8beafb56394403
Your Name <your.email@example.com>
2025-07-11 16:13:50 +0530

    deploy in render

--- backend/index.js
+++ backend/index.js
@@ -77,9 +77,9 @@ app.post("/generate-pdf", async (req, res) => {
 });
 
 
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server running at http://localhost:${PORT}`);
-// });
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
 
 module.exports = app
